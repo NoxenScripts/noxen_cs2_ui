a590f4d67684faf182cee521c41548aefc95fb15
Rubylium <19718604+Rubylium@users.noreply.github.com>
2024-10-10 13:06:42 +0200

    reorder manifest correctly

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,21 +6,10 @@ client_script "@STITGUARD/handler/stitguard.lua"
 fx_version 'adamant'
 games { 'gta5' }
 
-server_exports {
-    "AddEventForRessouurce",
-    "CheckEventIntegrity",
-}
 
 client_scripts {
-	'config/shared/**/*.lua',
     'config/client/**/*.lua',
     'client/**/*.lua',
-}
-
-server_scripts {
-    'config/shared/**/*.lua',
-    'config/server/**/*.lua',
-    'server/**/*.lua',
 }
 
 lua54 "yes"
