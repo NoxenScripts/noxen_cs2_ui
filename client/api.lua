2992af3bdd5f3400b3d2b16182714d05010d0271
Rubylium <19718604+Rubylium@users.noreply.github.com>
2024-10-10 13:17:09 +0200

    feat: Add API

---
+++ client/api.lua
@@ -0,0 +1,25 @@
+API = {}
+
+function API.SetScoreTeam1(score)
+    CS2_UI.TopScoresTeam1 = score
+end
+
+function API.SetScoreTeam2(score)
+    CS2_UI.TopScoresTeam2 = score
+end
+
+function API.SetTeam(team)
+    CS2_UI.CurrentTeam = team
+end
+
+function API.AddKill()
+    CS2_UI.PlayKillAnimation()
+end
+
+function API.ResetKills()
+    CS2_UI.Kills = 0
+end
+
+function API.Toggle()
+    CS2_UI.Toggle()
+end
